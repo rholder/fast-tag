3224e8a0dbf30328ee29aef4b9e2d9e1f9443c3e
Ray Holder <ray@blacklocus.com>
2015-03-06 01:10:17 -0600

    ensure Apache License 2.0 is applied to original dual-licensed code

--- fast-tag/src/main/java/com/knowledgebooks/nlp/util/Tokenizer.java
+++ fast-tag/src/main/java/com/knowledgebooks/nlp/util/Tokenizer.java
@@ -1,7 +1,18 @@
-// Copyright 2003-2008.  Mark Watson (markw@markwatson.com).  All rights reserved.
-// This software is released under the LGPL (www.fsf.org)
-// For an alternative non-GPL license: contact the author
-// THIS SOFTWARE COMES WITH NO WARRANTY
+/*
+ * Copyright 2003-2008 Mark Watson (markw@markwatson.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.knowledgebooks.nlp.util;
 
